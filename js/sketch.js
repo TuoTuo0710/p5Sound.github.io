5b2e591e2bfd10e39b74f64ac0ab55d354e96648
TuoTuo <kenny0408237@gmail.com>
2020-11-14 23:52:30 +0800

    Create audio visualizer.

---
+++ js/sketch.js
@@ -0,0 +1,23 @@
+let mic;
+
+function setup() {
+  // createCanvas(800, 500);
+  createCanvas(windowWidth, windowHeight);
+  xpos = width * 0.5;
+  ypos = height * 0.5;
+  mic = new p5.AudioIn();
+  mic.start();
+}
+
+function draw() {
+  background(40);
+  const vol = mic.getLevel();
+  const radius = map(vol, 0, 0.05, 10, 50);
+  noStroke();
+  fill(200, 190, 190);
+  ellipse(xpos, ypos, radius, radius);
+}
+
+function touchStarted() {
+    getAudioContext().resume();
+  }
